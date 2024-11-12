c92a11d8c5370d5dfe28db547c3c8a574e62c116
ankio <ankio@ankio.net>
2024-11-12 10:12:47 +0800

    :sparkles: (filepond): 添加 FilePond 库和 Image Preview 插件
    - 使用 FilePond 库和 Image Preview 插件来实现文件上传功能。
    - Image Preview 插件可以预览图片文件。
    - FilePond 提供多种文件上传方式，包括拖放、浏览和粘贴。
    - FilePond 的 API 提供多种方法来操作上传的文件，包括添加、删除、上传和准备输出。
    - FilePond 的状态管理系统可以跟踪文件上传的状态，并提供相关事件回调。
    - 该功能已添加到 FileUpload 组件中。

---
+++ FileUpload.js
@@ -0,0 +1,347 @@
+class FileUpload extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.innerHTML = `<input type="file"  />`;
+    // 初始化自定义的 validity 对象
+    this._validity = {
+      valueMissing: false,
+      customError: false,
+      valid: false,
+    };
+  }
+  // 自定义 validity 对象的 getter
+  get validity() {
+    return this._validity;
+  }
+
+
+  reportValidity() {
+    $.toaster.error("请上传文件或者等待文件上传完成");
+    let filepond = this.shadowRoot.querySelector(".filepond--drop-label");
+    filepond.classList.add("filepond--error");
+
+  }
+
+
+  // 验证方法
+  validate() {
+    let filepond = this.shadowRoot.querySelector(".filepond--drop-label");
+    filepond.classList.remove("filepond--error");
+
+
+    if (this.pond.status !== 4 && this.pond.status !== 1 && this.getAttribute("required") !== null) {
+      this._validity.valueMissing = false;
+      return;
+    }
+
+    // 检查是否为空
+    if (this.getAttribute("required") === null) {
+      this._validity.valueMissing = false;
+    } else {
+      this._validity.valueMissing = this._value.length === 0;
+    }
+    // 更新 valid 属性
+    this._validity.valid = !this._validity.valueMissing ;
+
+
+    console.log(this._validity.valid, this._value.length === 0)
+
+  }
+
+  get value() {
+    return this._value;
+  }
+
+    set value(val) {
+      this.setFiles(val);
+    }
+
+  connectedCallback() {
+    this.config = {
+      uri: this.getAttribute("uri") || "/upload",
+      allowMultiple: this.getAttribute("multiple") === "true",
+      name: this.getAttribute("name") || "file",
+      files: [], // [{uri: "http://xxx.com/xxx.jpg", name: "xxx.jpg", size: 1024, type: "image/jpeg"}]
+      maxFiles: this.getAttribute("limit") || 1,
+      accept: (this.getAttribute("accept") || "image/*").split(","),
+    };
+    this.init();
+  }
+
+  initComponent() {
+    FilePond.registerPlugin(FilePondPluginImagePreview);
+    FilePond.registerPlugin(FilePondPluginFileValidateType);
+//    FilePond.registerPlugin(FilePondPluginGPXAdvanced);
+    this.config.files = [];
+    this.initFilePond();
+  }
+
+  init() {
+    loader.load(
+      [
+        "/components/fileUpload/filepond.css",
+        "/components/fileUpload/filepond-plugin-file-validate-type.js",
+        "/components/fileUpload/filepond-plugin-image-preview.css",
+        "/components/fileUpload/filepond-plugin-image-preview.js",
+        "/components/fileUpload/filepond.js",
+        "/components/fileUpload/image-compressor.js",
+        "/components/fileUpload/FileUploader.js",
+      ],
+        () => {
+          this.initComponent();
+
+        },
+      this.shadowRoot,
+    );
+
+  }
+
+  addFileValue(val) {
+    if(this.config.allowMultiple){
+      if(this._value) {
+        this._value.push(val);
+      }else {
+        this._value = [val];
+      }
+    }else {
+      this._value = val;
+    }
+    this.validate();
+  }
+
+  setFiles(files) {
+    this.config.files = [];
+    files =  files.length > 0 ? files : [];
+    //判断files是否为String
+    if (typeof files === "string") {
+      files = [files];
+    }
+    files.forEach((file) => {
+      this.config.files.push({
+        source: file,
+        options: {
+          type: "local",
+        },
+      });
+    });
+    if (this.config.allowMultiple) {
+      this._value = files;
+    } else {
+      this._value = files.length > 0 ? files[0] : "";
+    }
+    this.pond.setOptions({
+      files: this.config.files,
+    });
+    this.validate();
+  }
+  removeFileValue(val) {
+    if(this.config.allowMultiple){
+      this._value = this._value.filter((item) => item !== val);
+    }else {
+      this._value = "";
+    }
+    this.validate();
+  }
+
+  initFilePond() {
+    const inputElement = this.shadowRoot.querySelector("input");
+    let that = this;
+    let config = {
+      name: this.config.name,
+      allowMultiple: this.config.allowMultiple,
+      maxFiles: this.config.maxFiles,
+      files: this.config.files,
+      credits: false,
+      server: {
+        process: (fieldName, file, metadata, load, error, progress, abort, transfer, options) => {
+          that._value = [];
+
+          const uploader = new FileUploader(file, {
+            chunkSize: 2 * 1024 * 1024, // 2MB per chunk
+            endpoint: that.config.uri,
+            onProgress: (current, total) => {
+
+              progress(true, current, total);
+            },
+            onComplete: (result) => {
+              load(result.data);
+              that.addFileValue(result.data);
+            },
+            onError: (error) => {
+              console.error('Upload failed', error);
+            }
+          });
+
+          uploader.upload();
+
+
+          return {
+            abort: () => {
+              // This function is entered if the user has tapped the cancel button
+              uploader.abort();
+
+              // Let FilePond know the request has been cancelled
+              abort();
+            },
+          };
+        },
+        load: (source, load, error, progress, abort, headers) => {
+          const uploader = new FileUploader(null, {
+            chunkSize: 2 * 1024 * 1024, // 2MB per chunk
+            endpoint: that.config.uri,
+
+            onComplete: (result) => {
+              load(result);
+              that.addFileValue(source);
+            },
+            onError: (error) => {
+              console.error('Upload failed', error);
+            }
+          });
+
+          uploader.load(source);
+
+
+          // Should expose an abort method so the request can be cancelled
+          return {
+            abort: () => {
+              // User tapped cancel, abort our ongoing actions here
+              uploader.abort();
+              // Let FilePond know the request has been cancelled
+              abort();
+            },
+          };
+        },
+        revert: (source, load, error) => {
+          // Should somehow send `source` to server so server can remove the file with this source
+
+          const uploader = new FileUploader(null, {
+            chunkSize: 2 * 1024 * 1024, // 2MB per chunk
+            endpoint: that.config.uri,
+
+          });
+
+          that.removeFileValue(source)
+          uploader.remove(source);
+          // Should call the load method when done, no parameters required
+          load();
+        },
+      },
+      //文件类型
+      acceptedFileTypes: this.config.accept,
+
+      // 用于上传按钮的标签。
+      fileValidateTypeDetectType: (source, type) =>
+        new Promise((resolve, reject) => {
+          if (!type) {
+            let name = source.name;
+            const match = name.match(/(\.[^.]+)$/);
+            type = match ? match[1] : type;
+          }
+
+          resolve(type);
+        }),
+    };
+    this.pond = FilePond.create(inputElement, config);
+    this.setFiles(this.config.files);
+    $.emitter.on("translate:set", () => {
+      that.setTranslate();
+    });
+    that.setTranslate();
+  }
+  setTranslate() {
+    let obj = {
+      labelFileTypeNotAllowed: "该文件类型不允许上传",
+      fileValidateTypeLabelExpectedTypes: "应当选择 {allTypes}",
+      labelIdle:
+        '拖放文件或 <span class="filepond--label-action"> 浏览 </span>',
+      // 显示的默认标签表明这是一个放置区域。FilePond 将自动将浏览文件事件绑定到 CSS 类 .filepond--label-action 的元素。
+
+      labelInvalidField: "字段包含无效的文件",
+      // 当字段包含无效文件并由父表单验证时显示的标签。
+
+      labelFileWaitingForSize: "等待文件大小信息",
+      // 等待文件大小信息时使用的标签。
+
+      labelFileSizeNotAvailable: "文件大小不可用",
+      // 未收到文件大小信息时使用的标签。
+
+      labelFileLoading: "加载中",
+      // 加载文件时使用的标签。
+
+      labelFileLoadError: "加载时出错",
+      // 文件加载失败时使用的标签。
+
+      labelFileProcessing: "上传中",
+      // 上传文件时使用的标签。
+
+      labelFileProcessingComplete: "上传完成",
+      // 文件上传完成时使用的标签。
+
+      labelFileProcessingAborted: "上传已取消",
+      // 取消上传时使用的标签。
+
+      labelFileProcessingError: "上传过程中出错",
+      // 文件上传过程中出现问题时使用的标签。
+
+      labelFileProcessingRevertError: "恢复时出错",
+      // 恢复文件上传过程中出现问题时使用的标签。
+
+      labelFileRemoveError: "删除时出错",
+      // 删除文件时出现问题的标签。
+
+      labelTapToCancel: "点击取消",
+      // 用于向用户指示可以取消操作的标签。
+
+      labelTapToRetry: "点击重试",
+      // 用于向用户指示可以重试操作的标签。
+
+      labelTapToUndo: "点击撤销",
+      // 用于向用户指示可以撤消操作的标签。
+
+      labelButtonRemoveItem: "删除",
+      // 用于删除按钮的标签。
+
+      labelButtonAbortItemLoad: "中止",
+      // 用于中止加载按钮的标签。
+
+      labelButtonRetryItemLoad: "重试",
+      // 用于重试加载按钮的标签。
+
+      labelButtonAbortItemProcessing: "取消",
+      // 用于中止上传按钮的标签。
+
+      labelButtonUndoItemProcessing: "撤销",
+      // 用于撤消上传按钮的标签。
+
+      labelButtonRetryItemProcessing: "重试",
+      // 用于重试上传按钮的标签。
+
+      labelButtonProcessItem: "上传",
+    };
+    let translates = [];
+    let that = this;
+
+    for (let key in obj) {
+      translates.push(obj[key]);
+    }
+    if ($.translate instanceof Function) {
+      $.translate(translates, (data) => {
+        let keys = Object.keys(obj);
+        data.forEach((res, index) => {
+          obj[keys[index]] = res;
+        });
+        that.pond.setOptions(obj);
+      });
+    }else {
+      that.pond.setOptions(obj);
+    }
+
+  }
+  destroy() {
+    this.pond.destroy();
+  }
+}
+
+customElements.define("mdui-file-upload", FileUpload);
---
+++ FileUploader.js
@@ -0,0 +1,165 @@
+class FileUploader {
+    constructor(file, options) {
+        this.file = file;
+        this.chunkSize = options.chunkSize || 2 * 1024 * 1024; // Default to 2MB per chunk
+        this.endpoint = options.endpoint || '/upload.php';
+        this.onProgress = options.onProgress || function () {
+            console.info("Uploading...", arguments);
+        };
+        this.onComplete = options.onComplete || function () { };
+        this.onError = options.onError || function () { };
+        this.uploadId = options.uploadId || this.generateUploadId();
+        this.currentChunk = 0;
+        this.ajax = null;
+    }
+
+    generateUploadId() {
+        return Math.random().toString(36).substring(2, 16);
+    }
+
+    upload() {
+        this.totalChunks = Math.ceil(this.file.size / this.chunkSize);
+
+        if (!this.file.type || this.file.type.indexOf("image") === -1) {
+            this.uploadChunk("");
+            return;
+        }
+        let that = this;
+        $.loader("/components/fileUpload/image-compressor.js", () => {
+            const options = {
+                file: that.file,
+                convertSize: 1024 * 512, // 512KB
+                quality: 0.6, // 80%
+                maxWidth: 1024,
+                // Before compression callback
+                beforeCompress: function (result) {
+                    console.info("Original image size: ", result.size);
+                    console.info("MIME type: ", result.type);
+                },
+
+                // Success callback
+                success: function (result) {
+                    console.info("Compressed image size: ", result.size);
+                    console.info("MIME type: ", result.type);
+                    console.info(
+                        "Compression ratio: ",
+                        (
+                            ((that.file.size - result.size) / that.file.size) *
+                            100
+                        ).toFixed(2) + "%",
+                    );
+                    that.file = result;
+                    that.totalChunks = Math.ceil(result.size / that.chunkSize);
+                    that.uploadChunk("");
+                },
+            };
+
+            new ImageCompressor(options);
+        });
+    }
+
+    abort() {
+        if (this.ajax) {
+            this.ajax.abort();
+        }
+    }
+
+
+    remove(name) {
+        this.ajax = new XMLHttpRequest();
+        this.ajax.open('DELETE', `${this.endpoint}/${encodeURIComponent(name)}`, true);
+        for (let key in headers) {
+            if (headers.hasOwnProperty(key)) {
+                this.ajax.setRequestHeader(key, headers[key]);
+            }
+        }
+        this.ajax.send();
+    }
+
+    load(name) {
+        this.ajax = new XMLHttpRequest();
+        this.ajax.open('GET', `${this.endpoint}/${encodeURIComponent(name)}`, true);
+
+        for (let key in headers) {
+            if (headers.hasOwnProperty(key)) {
+                this.ajax.setRequestHeader(key, headers[key]);
+            }
+        }
+        this.ajax.responseType = "blob";
+        this.ajax.onload = () => {
+            if (this.ajax.status >= 200 && this.ajax.status < 300) {
+                const blob = this.ajax.response;
+                const file = new File([blob], name, { type: blob.type });
+                this.onComplete(file);
+            } else {
+                this.onError(this.ajax.statusText);
+                console.error("Load failed");
+            }
+        };
+
+        this.ajax.onerror = () => {
+            this.onError(this.ajax.statusText);
+            console.error("Load failed");
+        };
+
+        this.ajax.send();
+    }
+
+    uploadChunk(uniqueID) {
+        const start = this.currentChunk * this.chunkSize;
+        const end = Math.min(start + this.chunkSize, this.file.size);
+        const chunk = this.file.slice(start, end);
+        const formData = new FormData();
+
+        formData.append('file', chunk);
+        formData.append('uploadId', this.uploadId);
+        formData.append('chunkIndex', this.currentChunk);
+        formData.append('totalChunks', this.totalChunks);
+        formData.append('fileName', this.file.name);
+        formData.append("unique", uniqueID);
+
+        this.ajax = new XMLHttpRequest();
+        this.ajax.open('POST', this.endpoint, true);
+
+        for (let key in headers) {
+            if (headers.hasOwnProperty(key)) {
+                this.ajax.setRequestHeader(key, headers[key]);
+            }
+        }
+
+        this.ajax.upload.onprogress = (evt) => {
+            if (evt.lengthComputable) {
+                const chunkProgress = (evt.loaded / evt.total);
+                this.onProgress(this.currentChunk + chunkProgress, this.totalChunks);
+            }
+        };
+
+        this.ajax.onload = () => {
+            if (this.ajax.status >= 200 && this.ajax.status < 300) {
+                const result = JSON.parse(this.ajax.responseText);
+                if (result.code === 200) {
+                    this.currentChunk++;
+                    if (this.currentChunk < this.totalChunks) {
+                        this.uploadChunk(uniqueID);
+                    } else {
+                        this.onComplete(result);
+                    }
+                } else {
+                    this.onError(result.msg);
+                }
+            } else {
+                this.onError(this.ajax.statusText);
+                console.error("Upload failed");
+            }
+        };
+
+        this.ajax.onerror = () => {
+            this.onError(this.ajax.statusText);
+            console.error("Upload failed");
+        };
+
+        this.ajax.send(formData);
+    }
+
+
+}
